2443231b08ff02c014a45200d9aad27ba2149f06
Hungng <hungnguyen35800@gmail.com>
2021-08-12 22:11:04 +0700

    a tag to new tab

--- src/features/oneTask/Task.js
+++ src/features/oneTask/Task.js
@@ -40,7 +40,7 @@ const Task = () => {
         if (text != null) {
             var urlRegex = new RegExp("((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[\\w]*))?)");
             const replaced = text.replace(urlRegex, function (url) {
-                return '<a href="' + url + '">' + url + '</a>';
+                return '<a target="_blank" rel="noopener noreferrer" href="' + url + '">' + url + '</a>';
             })
             return replaced
         }
